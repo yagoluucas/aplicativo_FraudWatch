9326db5fd0c989cd19e6aac192eed24aa8b5697c
yagoluucas <yago543@gmail.com>
2024-10-08 22:35:57 -0300

    finalizado processo de passar o objeto user para as activity e inicializado validações de campos

--- app/src/main/java/com/example/fraud_watch/activity/AddressActivity.kt
+++ app/src/main/java/com/example/fraud_watch/activity/AddressActivity.kt
@@ -7,9 +7,11 @@ import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fraud_watch.R
 import com.example.fraud_watch.fragments.BtnCloseCadastro
+import com.example.fraud_watch.model.Address
 import com.example.fraud_watch.model.User
 
 class AddressActivity: AppCompatActivity() {
@@ -19,7 +21,13 @@ class AddressActivity: AppCompatActivity() {
 
         val btnFinalizarCadastro: Button = findViewById(R.id.Endereco_BtnAvancar_Button)
         val user: User? = intent.getSerializableExtra("user") as? User
-
+        val campoCep: EditText = findViewById(R.id.Endereco_CampoCep_EditText)
+        val campoBairro: AutoCompleteTextView = findViewById(R.id.Endereco_DropDowBairro_AutoComplete)
+        val campoCidade: AutoCompleteTextView = findViewById(R.id.Endereco_DropDowCidade_AutoComplete)
+        val campoEstado: AutoCompleteTextView = findViewById(R.id.Endereco_DropDowEstado_AutoComplete)
+        val campoRua: EditText = findViewById(R.id.Endereco_CampoRua_EditText)
+        val campoNumero: EditText = findViewById(R.id.Endereco_CampoNumero_EditText)
+        val campoComplemento: EditText = findViewById(R.id.Endereco_CampoComplemento_EditText)
 
         if (savedInstanceState == null) {
             // Adiciona o fragmento ao container (frame layout)
@@ -69,7 +77,18 @@ class AddressActivity: AppCompatActivity() {
         autoCompleteEstado.setAdapter(adapterEstado)
 
         btnFinalizarCadastro.setOnClickListener{
+            val endereco: Address = Address(
+                campoCep.text.toString(),
+                campoEstado.text.toString(),
+                campoCidade.text.toString(),
+                campoBairro.text.toString(),
+                campoRua.text.toString(),
+                campoNumero.text.toString(),
+                campoComplemento.text.toString())
+
+            user?.address = endereco
             val intent = Intent(this, FinishActivityRegistration::class.java)
+            intent.putExtra("user", user)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/fraud_watch/activity/FinishActivityRegistration.kt
+++ app/src/main/java/com/example/fraud_watch/activity/FinishActivityRegistration.kt
@@ -1,10 +1,14 @@
 package com.example.fraud_watch.activity
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fraud_watch.R
 import com.example.fraud_watch.fragments.BtnCloseCadastro
+import com.example.fraud_watch.model.User
+import com.example.fraud_watch.utils.Utils
 
 class FinishActivityRegistration: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +22,30 @@ class FinishActivityRegistration: AppCompatActivity() {
         }
 
         val btnFinalizarCadastro: Button = findViewById(R.id.Finish_BtnFinalizarCadastro_Button)
+        val user: User? = intent.getSerializableExtra("user") as? User
+        val campoEmail: EditText = findViewById(R.id.Finish_CampoEmail_EditText)
+        val campoTelefone: EditText = findViewById(R.id.Finish_CampoTelefone_EditText)
+        val campoConfirmarSenha: EditText = findViewById(R.id.Finish_CampoComfirmarSenha_EditText)
+        val campoSenha: EditText = findViewById(R.id.Finish_CampoSenha_EditText)
 
         btnFinalizarCadastro.setOnClickListener{
-
+            validaCampos(campoEmail, campoTelefone, campoSenha, campoConfirmarSenha)
+            user?.email = campoEmail.text.toString()
+            user?.senha = campoConfirmarSenha.text.toString()
+            user?.telefone = campoTelefone.text.toString()
+            Log.v("usuário finalizado", user.toString())
         }
+    }
+
+    fun validaCampos(campoEmail: EditText,
+                     campoTelefone: EditText,
+                     campoSenha: EditText,
+                     campoConfirmarSenha: EditText): Boolean{
+
+        if(!android.util.Patterns.EMAIL_ADDRESS.matcher(campoEmail.text.toString()).matches()){
+            campoEmail.error = "Email inválido!"
+            return false
+        }
+        return true
     }
 }
