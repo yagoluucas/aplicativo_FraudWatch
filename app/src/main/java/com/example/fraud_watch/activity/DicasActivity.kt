2a039bca5f05c49fb12ad7f78a7a7affbfa587e2
yagoluucas <yago543@gmail.com>
2024-10-24 15:10:00 -0300

    Feito escopo inicial da página de dicas

--- app/src/main/java/com/example/fraud_watch/activity/DicasActivity.kt
+++ app/src/main/java/com/example/fraud_watch/activity/DicasActivity.kt
@@ -1,6 +1,8 @@
 package com.example.fraud_watch.activity
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,10 +14,22 @@ class DicasActivity : AppCompatActivity() {
         overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dicas)
+
+        val primeiroTituloEvitarFraudes: TextView = findViewById(R.id.Dicas_TituloDicasEvitarFraude1_TextView)
+        val primeiraDescricaoEvitarFraudes: TextView = findViewById(R.id.Dicas_DescricaoDicasEvitarFraude1_TextView)
+
+        primeiroTituloEvitarFraudes.setOnClickListener {
+            mostrarDescricaoDoTitulo(primeiraDescricaoEvitarFraudes)
+        }
     }
 
     override fun onResume() {
         super.onResume()
         overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+    }
+
+    fun mostrarDescricaoDoTitulo(campoDescricao: TextView){
+        if (campoDescricao.visibility == View.GONE) campoDescricao.visibility = View.VISIBLE
+        else campoDescricao.visibility = View.GONE
     }
 }
